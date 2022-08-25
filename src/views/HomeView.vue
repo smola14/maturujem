ecc4ce4d2e2281713a836b186f1844acb24c59a3
smola14 <dominik.smolinsky@icloud.com>
2022-08-25 11:28:24 +0200

    first design prototype

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <img src="@/img.png" alt="img" />
+  </div>
 </template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+</style>
